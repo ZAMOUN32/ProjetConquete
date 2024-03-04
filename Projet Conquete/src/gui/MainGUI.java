8d180ace1b31155af3485c91736db20431a3f3cc
Sdiakho <161468907+Sdiakho@users.noreply.github.com>
2024-03-05 00:11:15 +0100

    Update MainGUI.java

--- Projet Conquete/src/gui/MainGUI.java
+++ Projet Conquete/src/gui/MainGUI.java
@@ -125,7 +125,7 @@ public class MainGUI extends JFrame implements Runnable {
 		public void mouseClicked(MouseEvent e) {
 			int x = e.getX();
 			int y = e.getY();
-			Block batimentpos = dashbord.getBatiments(x,y);
+			Block batimentpos = dashbord.getBatiment(x,y);
 			buldingmanager.putUnites(batimentpos);
 			
 		}
